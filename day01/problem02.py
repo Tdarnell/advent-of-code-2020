24d957d21f95a2173b99c7cb03a95098c42df5cf
thomasatdarnell@gmail.com <thomasatdarnell@gmail.com>
2020-12-03 17:03:31 +0000

    improve comments slightly...they're still rushed 🤷‍♂️

--- day01/problem02.py
+++ day01/problem02.py
@@ -1,9 +1,11 @@
 import numpy as np
 # import the data
 infile = np.genfromtxt("day01/input.txt", dtype=int)
-# produce an array of 2020-the input
+# produce a sum product array
 sum_array1 = np.array([infile + n for n in infile])
+# subtract 2020 from all sum products
 search_arr = (sum_array1-2020)*-1
+# Find the duplicate between the input array and the sum product - 2020
 intersects = np.intersect1d(infile, search_arr)
 # product of the intersects
 prod = np.prod(intersects)
